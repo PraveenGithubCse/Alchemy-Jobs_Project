2a5d708672a2990c72143853c905312178382f1c
Nandana Jacob <nandana.jacob@ibm.com>
2025-06-24 12:40:27 +0530

    modified Activity3 and Activity4

--- Coding Part (Selenium with TestNG)/Activity3.java
+++ Coding Part (Selenium with TestNG)/Activity3.java
@@ -1,21 +1,34 @@
-package Project;
+package alchemyJobs;
+
+//import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Activity3 {
-public static void main(String[] args) {
-
-
-	
-	WebDriver driver=new FirefoxDriver();
-	driver.get("https://alchemy.hguy.co/jobs/");
-	 WebElement img=driver.findElement(By.cssSelector(".attachment-large.size-large.wp-post-image"));
-	String imgUrl = img.getAttribute("src");
-	System.out.println("Image URL: " + imgUrl);
-	driver.close();
-}
-	
+	WebDriver driver = new FirefoxDriver();
+	@BeforeClass
+	public void setUp()
+	{
+		driver.get("https://alchemy.hguy.co/jobs/");
+		
+	}
+	// Verify website title
+	@Test(priority = 1)
+	public void displayImageSrc()
+	{
+		WebElement img=driver.findElement(By.cssSelector(".attachment-large.size-large.wp-post-image"));
+		String imgUrl = img.getAttribute("src");
+		System.out.println("Image URL: " + imgUrl);
+		Reporter.log(imgUrl);
+		driver.quit();
+	}
 }
--- Coding Part (Selenium with TestNG)/Sample.java
+++ Coding Part (Selenium with TestNG)/Sample.java
@@ -1,41 +1,37 @@
 package alchemyJobs;
 
-import static org.testng.Assert.assertEquals;
-
-//import static org.testng.Assert.assertEquals;
 
 import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
-public class Activity4 {
+public class Sample {
 	WebDriver driver;
 	WebDriverWait wait;
 	@BeforeClass
 	public void setUp()
 	{
-		driver = new FirefoxDriver();
+		driver = new EdgeDriver();
 		wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		driver.get("https://alchemy.hguy.co/jobs/");
 		
 	}
+	// Verify website title
 	@Test(priority = 1)
 	public void verifyTitle()
 	{
-		String headingText = driver.findElement(By.xpath("//h2")).getText();
-		assertEquals(headingText, "Quia quis non");
+	  assertEquals(driver.getTitle(), "Alchemy Jobs – Job Board Application");
+	  driver.quit();
 	}
-	@AfterClass
-	public void tearDown()
-	{
-		driver.quit();
-		
-	}
+	
+	
+	
+
 }
+
